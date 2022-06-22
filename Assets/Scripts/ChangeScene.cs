c58339c959181fffa1a680086a9f8b7ea1828982
mametubu <supera7819@gmail.com>
2022-06-22 15:35:06 +0900

    追加:TitleからMainへのシーン以降のスクリプト

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
