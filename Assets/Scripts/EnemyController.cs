056837e06d87c47df7078d07e98c22d3a3514960
mametubu <supera7819@gmail.com>
2022-07-03 03:35:04 +0900

    追加:敵の攻撃判定

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵の動きに関するスクリプト
+/// </summary>
+public class EnemyController : MonoBehaviour
+{
+
+    [SerializeField]
+    private int maxHitPoint;
+    [SerializeField]
+    private int basicDamage;
+    [SerializeField]
+    private int enemyAttackCount;
+
+    [SerializeField]
+    private GameObject touchManager;
+
+    private int hitPoint;
+    private int hitPointPercent;
+    private int changePercent = 100;
+
+    public int attackDamage;
+
+    void Start()
+    {
+        StartCoroutine(EnemyAttack());
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    IEnumerator EnemyAttack()
+    {
+        hitPointPercent = hitPoint / maxHitPoint * changePercent;
+        if(hitPointPercent >= 75)
+        {
+            attackDamage += basicDamage * enemyAttackCount;
+        }
+        else if(hitPointPercent >= 50)
+        {
+            attackDamage += basicDamage * (enemyAttackCount + 1);
+        }
+        else if(hitPointPercent >= 5)
+        {
+            attackDamage += basicDamage * (enemyAttackCount + 2);
+        }
+        else
+        {
+            attackDamage += basicDamage * (enemyAttackCount + 10);
+        }
+        yield return null;
+    }
+}
